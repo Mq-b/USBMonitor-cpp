0ce51e10bf36ce2fbaf9f7aadbe86a66d60e0fa5
mq-b <3326284481@qq.com>
2025-01-08 10:39:22 +0800

    更新项目，还未编写 CMake 以及测试 demo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# USBWatch-cpp
-USBWatch-cpp is an open-source project written in C++ that provides a library for real-time monitoring of USB device insertion and removal status.
+# USBMonitor-cpp
+
+`USBMonitor-cpp` 是一个用 C++17 编写的开源项目，提供了一个用于实时监控 USB 设备插拔状态的库。
+
+它十分的简单，基于标准 C++，通过开启一个线程时刻监控 Linux 中外部存储设备默认挂载路径来发送信号。
+
+在 Ubuntu22.04 中使用 GCC11、GCC12 测试无误。
---
+++ include/USBMonitor.h
@@ -0,0 +1,59 @@
+#ifndef USBMONITOR_H
+#define USBMONITOR_H
+
+#include <set>
+#include <filesystem>
+#include <fstream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <chrono>
+#include <functional>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+enum UsbState {
+    Removed,    // U盘拔出
+    Inserted,   // U盘插入
+    UpdateReady // 是一个更新U盘（有 update 文件夹且不为空）
+};
+
+class USBMonitor {
+public:
+    explicit USBMonitor(const std::function<void(UsbState,std::string)>& state);
+    ~USBMonitor();
+    void startMonitoring();
+
+    static bool isDirectoryNotEmpty(const std::filesystem::path& path) {
+        // 检查路径是否存在且为目录
+        if (std::filesystem::exists(path) && std::filesystem::is_directory(path)) {
+            // 如果是目录，检查是否为空
+            auto it = std::filesystem::directory_iterator(path);
+            return it != std::filesystem::end(it); // 如果目录不为空，返回 true
+        }
+        return false;
+    }
+    static bool isUSBMounted(const std::string& path) {
+        std::ifstream mounts("/proc/mounts");
+        std::string line;
+        while (std::getline(mounts, line)) {
+            if (line.find(path) != std::string::npos) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    void monitorUSB();
+    void findUSBMountPaths();
+
+    std::set<std::string> currentUSBPaths; // 记录当前 USB 路径
+    mutable std::mutex m;
+    std::atomic_bool stop = false;
+    std::function<void(UsbState,std::string)> USBstate; // 用于外部注入代码，发送插入与拔出的信号
+    std::thread usbMonitorThread;
+};
+
+#endif // USBMONITOR_H
