33ef41737f3fc42ef3411373418df53412e90ba9
Ll0veU <xyz4821@163.com>
2025-09-10 00:23:22 +0800

    feat: 由于macOS原生QMessagebox不支持title显示，于是增加自定义对话框显示title。

--- examples/Qt-usb-monitor.cpp
+++ examples/Qt-usb-monitor.cpp
@@ -3,6 +3,32 @@
 #include <QObject>
 #include <QMessageBox>
 
+#if defined(__APPLE__) && defined(__MACH__)
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+
+// macOS原生 QMessageBox 不显示 title，于是自定义对话框
+class UsbEventDialog : public QDialog {
+    Q_OBJECT
+public:
+    UsbEventDialog(const QString& title, const QString& message, QWidget* parent = nullptr)
+        : QDialog(parent) {
+        setWindowTitle(title);
+        setModal(true);
+        QVBoxLayout* layout = new QVBoxLayout(this);
+        QLabel* label = new QLabel(message, this);
+        layout->addWidget(label);
+        QPushButton* btn = new QPushButton("OK", this);
+        layout->addWidget(btn);
+        connect(btn, &QPushButton::clicked, this, &QDialog::accept);
+        setLayout(layout);
+        resize(320, 120);
+    }
+};
+#endif
+
 class UsbWatcher : public QObject {
     Q_OBJECT
 public:
@@ -21,7 +47,12 @@ signals:
 public slots:
     void onUsbEvent(int state, const QString& path) {
         static const char* name[] = { "USB Removed", "USB Inserted", "USB Update Ready" };
+#if defined(__APPLE__) && defined(__MACH__)
+        UsbEventDialog dlg(QString::fromUtf8(name[state]), path);
+        dlg.exec();
+#else
         QMessageBox::information(nullptr, name[state], path);
+#endif
     }
 
 private:
